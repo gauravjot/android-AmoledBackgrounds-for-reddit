199a26335616ae55373a98c03a7191ca299ab674
Gauravjot Garaya <gauravjot@outlook.com>
2024-02-07 13:14:56 -0800

    working release on Android U

--- app/src/main/java/com/droidheat/amoledbackgrounds/DownloadsFragment.java
+++ app/src/main/java/com/droidheat/amoledbackgrounds/DownloadsFragment.java
@@ -2,6 +2,7 @@ package com.droidheat.amoledbackgrounds;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,53 +19,53 @@ import androidx.fragment.app.Fragment;
 import com.droidheat.amoledbackgrounds.adapters.MyDownloadsAdapter;
 
 public class DownloadsFragment extends Fragment {
-
-  public DownloadsFragment() {
-  }
-
-  MyDownloadsAdapter myDownloadsAdapter;
-  GridView gridView;
-  Button permissionBtn;
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_downloads, container, false);
-
-    gridView = view.findViewById(R.id.gridView);
-    permissionBtn = view.findViewById(R.id.button_permission);
-
-    myDownloadsAdapter = new MyDownloadsAdapter(getActivity());
-    gridView.setAdapter(myDownloadsAdapter);
-
-    final String PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE;
-
-    if (ContextCompat.checkSelfPermission(requireActivity(), PERMISSION) != PackageManager.PERMISSION_GRANTED) {
-      gridView.setVisibility(View.INVISIBLE);
-      requestPermissionLauncher.launch(PERMISSION);
-    } else {
-      permissionBtn.setVisibility(View.INVISIBLE);
-      gridView.setVisibility(View.VISIBLE);
-    }
-
-    permissionBtn.setOnClickListener(v -> requestPermissionLauncher.launch(PERMISSION));
-
-    return view;
-  }
-
-  private final ActivityResultLauncher<String> requestPermissionLauncher =
-          registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-            if (isGranted) {
-              permissionBtn.setVisibility(View.INVISIBLE);
-              gridView.setVisibility(View.VISIBLE);
-            } else {
-              Toast.makeText(getActivity(), "Read Permission Denied", Toast.LENGTH_SHORT).show();
-              // Explain to the user that the feature is unavailable because the
-              // features requires a permission that the user has denied. At the
-              // same time, respect the user's decision. Don't link to system
-              // settings in an effort to convince the user to change their
-              // decision.
-            }
-          });
-
+	
+	GridView gridView;
+	Button permissionBtn;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_downloads, container, false);
+		
+		gridView = view.findViewById(R.id.gridView);
+		permissionBtn = view.findViewById(R.id.button_permission);
+		
+		// Choose appropriate permission based on Android version
+		String PERMISSION;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+			PERMISSION = Manifest.permission.READ_MEDIA_IMAGES;
+		} else {
+			PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE;
+		}
+		// Check if permission is granted
+		if (ContextCompat.checkSelfPermission(requireActivity(), PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+			// Hide empty gridview
+			gridView.setVisibility(View.INVISIBLE);
+			// Request permission
+			requestPermissionLauncher.launch(PERMISSION);
+		} else {
+			// Permission already granted; load images
+			loadImages();
+		}
+		
+		permissionBtn.setOnClickListener(v -> requestPermissionLauncher.launch(PERMISSION));
+		
+		return view;
+	}
+	
+	private void loadImages() {
+		MyDownloadsAdapter myDownloadsAdapter = new MyDownloadsAdapter(getActivity());
+		gridView.setAdapter(myDownloadsAdapter);
+		permissionBtn.setVisibility(View.INVISIBLE);
+		gridView.setVisibility(View.VISIBLE);
+	}
+	
+	private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+		if (isGranted) {
+			loadImages();
+		} else {
+			Toast.makeText(requireActivity(), "Read Permission Denied. If you wish, you can go into Settings > Apps > AmoledBackgrounds > Permissions.", Toast.LENGTH_LONG).show();
+		}
+	});
+	
 }
--- app/src/main/java/com/droidheat/amoledbackgrounds/MainActivity.java
+++ app/src/main/java/com/droidheat/amoledbackgrounds/MainActivity.java
@@ -27,153 +27,153 @@ import android.view.MenuItem;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-  private ViewPager viewPager;
-  MenuItem prevMenuItem;
-
-  private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-          = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-      int itemId = item.getItemId();
-      if (itemId == R.id.navigation_home) {
-        viewPager.setCurrentItem(0);
-        return true;
-      } else if (itemId == R.id.navigation_settings) {
-        viewPager.setCurrentItem(2);
-        return true;
-      } else if (itemId == R.id.navigation_notifications) {
-        viewPager.setCurrentItem(1);
-        return true;
-      }
-      return false;
-    }
-  };
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    Toolbar toolbar = findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-    Objects.requireNonNull(getSupportActionBar()).setTitle("Amoled Backgrounds");
-
-    try {
-      SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
-      PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-      boolean showChangelog = sharedPref.getBoolean("changelog" + pInfo.versionName, true);
-      if (showChangelog) {
-        (new AppUtils()).changelog(this).show();
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putBoolean("changelog" + pInfo.versionName, false);
-        editor.apply();
-      }
-    } catch (PackageManager.NameNotFoundException e) {
-      e.printStackTrace();
-    }
-
-    viewPager = findViewById(R.id.viewpager);
-    setupViewPager(viewPager);
-    viewPager.setOffscreenPageLimit(3);
-    final BottomNavigationView navigation = findViewById(R.id.navigation);
-    navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-    viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-      @Override
-      public void onPageScrolled(int i, float v, int i1) {
-
-      }
-
-      @Override
-      public void onPageSelected(int i) {
-        if (prevMenuItem != null)
-          prevMenuItem.setChecked(false);
-        else
-          navigation.getMenu().getItem(0).setChecked(false);
-
-        if (i == 2) {
-          Objects.requireNonNull(getSupportActionBar()).setTitle("Settings");
-        } else if (i == 1) {
-          Objects.requireNonNull(getSupportActionBar()).setTitle("Downloads");
-        } else {
-          Objects.requireNonNull(getSupportActionBar()).setTitle("Amoled Backgrounds");
-        }
-
-        navigation.getMenu().getItem(i).setChecked(true);
-        prevMenuItem = navigation.getMenu().getItem(i);
-      }
-
-      @Override
-      public void onPageScrollStateChanged(int i) {
-
-      }
-    });
-  }
-
-  @Override
-  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-  }
-
-  private void setupViewPager(ViewPager viewPager) {
-    ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-    Fragment wallpaperFragment = new WallpaperFragment();
-    Fragment settingsFragment = new SettingsFragment();
-    Fragment downloadsFragment = new DownloadsFragment();
-    adapter.addFragment(wallpaperFragment);
-    adapter.addFragment(downloadsFragment);
-    adapter.addFragment(settingsFragment);
-    viewPager.setAdapter(adapter);
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.menu, menu);
-    final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-
-    assert searchView != null;
-    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-      @SuppressLint("RestrictedApi")
-      @Override
-      public boolean onQueryTextSubmit(String s) {
-        Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-        intent.putExtra(SearchManager.QUERY, s);
-        startActivity(intent);
-        Objects.requireNonNull(getSupportActionBar()).collapseActionView();
-        return true;
-      }
-
-      @Override
-      public boolean onQueryTextChange(String s) {
-        return false;
-      }
-    });
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == R.id.action_rate) {
-      Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(this.getString(R.string.play_store_url)));
-      startActivity(browserIntent);
-    } else if (item.getItemId() == R.id.action_droidheat) {
-      Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://droidheat.com"));
-      startActivity(browserIntent);
-    } else if (item.getItemId() == R.id.action_share) {
-      Intent sendIntent = new Intent();
-      sendIntent.setAction(Intent.ACTION_SEND);
-      sendIntent.putExtra(Intent.EXTRA_TEXT,
-              "Hey! Check out this android app at: " + this.getString(R.string.play_store_url));
-      sendIntent.setType("text/plain");
-      startActivity(sendIntent);
-    }
+	
+	private ViewPager viewPager;
+	MenuItem prevMenuItem;
+	
+	private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+					= new BottomNavigationView.OnNavigationItemSelectedListener() {
+		
+		@Override
+		public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+			int itemId = item.getItemId();
+			if (itemId == R.id.navigation_home) {
+				viewPager.setCurrentItem(0);
+				return true;
+			} else if (itemId == R.id.navigation_settings) {
+				viewPager.setCurrentItem(2);
+				return true;
+			} else if (itemId == R.id.navigation_notifications) {
+				viewPager.setCurrentItem(1);
+				return true;
+			}
+			return false;
+		}
+	};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		Objects.requireNonNull(getSupportActionBar()).setTitle("Amoled Backgrounds");
+		
+		try {
+			SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
+			PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			boolean showChangelog = sharedPref.getBoolean("changelog" + pInfo.versionName, true);
+			if (showChangelog) {
+				(new AppUtils()).changelog(this).show();
+				SharedPreferences.Editor editor = sharedPref.edit();
+				editor.putBoolean("changelog" + pInfo.versionName, false);
+				editor.apply();
+			}
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		viewPager = findViewById(R.id.viewpager);
+		setupViewPager(viewPager);
+		viewPager.setOffscreenPageLimit(3);
+		final BottomNavigationView navigation = findViewById(R.id.navigation);
+		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+		
+		viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			@Override
+			public void onPageScrolled(int i, float v, int i1) {
+			
+			}
+			
+			@Override
+			public void onPageSelected(int i) {
+				if (prevMenuItem != null)
+					prevMenuItem.setChecked(false);
+				else
+					navigation.getMenu().getItem(0).setChecked(false);
+				
+				if (i == 2) {
+					Objects.requireNonNull(getSupportActionBar()).setTitle("Settings");
+				} else if (i == 1) {
+					Objects.requireNonNull(getSupportActionBar()).setTitle("Downloads");
+				} else {
+					Objects.requireNonNull(getSupportActionBar()).setTitle("Amoled Backgrounds");
+				}
+				
+				navigation.getMenu().getItem(i).setChecked(true);
+				prevMenuItem = navigation.getMenu().getItem(i);
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int i) {
+			
+			}
+		});
+	}
+	
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+	}
+	
+	private void setupViewPager(ViewPager viewPager) {
+		ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+		Fragment wallpaperFragment = new WallpaperFragment();
+		Fragment settingsFragment = new SettingsFragment();
+		Fragment downloadsFragment = new DownloadsFragment();
+		adapter.addFragment(wallpaperFragment);
+		adapter.addFragment(downloadsFragment);
+		adapter.addFragment(settingsFragment);
+		viewPager.setAdapter(adapter);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+		
+		assert searchView != null;
+		searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+			@SuppressLint("RestrictedApi")
+			@Override
+			public boolean onQueryTextSubmit(String s) {
+				Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+				intent.putExtra(SearchManager.QUERY, s);
+				startActivity(intent);
+				Objects.requireNonNull(getSupportActionBar()).collapseActionView();
+				return true;
+			}
+			
+			@Override
+			public boolean onQueryTextChange(String s) {
+				return false;
+			}
+		});
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.action_rate) {
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(this.getString(R.string.play_store_url)));
+			startActivity(browserIntent);
+		} else if (item.getItemId() == R.id.action_github) {
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/gauravjot/android-AmoledBackgrounds-for-reddit"));
+			startActivity(browserIntent);
+		} else if (item.getItemId() == R.id.action_share) {
+			Intent sendIntent = new Intent();
+			sendIntent.setAction(Intent.ACTION_SEND);
+			sendIntent.putExtra(Intent.EXTRA_TEXT,
+							"Hey! Check out this android app at: " + this.getString(R.string.play_store_url));
+			sendIntent.setType("text/plain");
+			startActivity(sendIntent);
+		}
 //        else if (item.getItemId() == R.id.action_auto_wallpaper) {
 //            Intent intent = new Intent(MainActivity.this,AutomaticWallpaperActivity.class);
 //            startActivity(intent);
 //        }
-    return true;
-  }
+		return true;
+	}
 }
